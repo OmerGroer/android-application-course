7c87ee8b7161d96497a37f536f82eb2fe56ec7bc
Omer Groer <drutr123@gmail.com>
2025-01-15 17:03:36 +0200

    student list with firebase

--- app/src/main/java/com/example/android_application_course/adapter/StudentViewHolder.kt
+++ app/src/main/java/com/example/android_application_course/adapter/StudentViewHolder.kt
@@ -2,48 +2,62 @@ package com.example.android_application_course.adapter
 
 import android.view.View
 import android.widget.CheckBox
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android_application_course.OnItemClickListener
+import com.bumptech.glide.Glide
+import com.example.android_application_course.fragments.studentsList.OnItemClickListener
 import com.example.android_application_course.R
-import com.example.android_application_course.model.Student
+import com.example.android_application_course.data.model.Student
+import com.example.android_application_course.fragments.studentsList.StudentsListViewModel
+import com.google.android.material.progressindicator.CircularProgressIndicator
 
 class StudentViewHolder(
     itemView: View,
-    listener: OnItemClickListener?
-): RecyclerView.ViewHolder(itemView) {
+    listener: OnItemClickListener?,
+    private val viewModel: StudentsListViewModel
+) : RecyclerView.ViewHolder(itemView) {
 
-    private var nameTextView: TextView? = null
-    private var idTextView: TextView? = null
-    private var studentCheckBox: CheckBox? = null
+    private var nameTextView: TextView = itemView.findViewById(R.id.student_row_name_text_view)
+    private var idTextView: TextView = itemView.findViewById(R.id.student_row_id_text_view)
+    private var studentCheckBox: CheckBox = itemView.findViewById(R.id.student_row_check_box)
+    private var avatar: ImageView = itemView.findViewById(R.id.student_row_image_view)
+    private var progressBar: CircularProgressIndicator = itemView.findViewById(R.id.progress_bar)
     private var student: Student? = null
 
     init {
-        nameTextView = itemView.findViewById(R.id.student_row_name_text_view)
-        idTextView = itemView.findViewById(R.id.student_row_id_text_view)
-        studentCheckBox = itemView.findViewById(R.id.student_row_check_box)
-
-        studentCheckBox?.apply {
+        studentCheckBox.apply {
             setOnClickListener {
                 (tag as? Int)?.let { _ ->
-                    student?.isChecked = (it as? CheckBox)?.isChecked ?: false
+                    val student = student ?: return@setOnClickListener
+                    viewModel.checkStudent(student.id, (it as? CheckBox)?.isChecked ?: false) {
+                    }
                 }
             }
-        }
 
-        itemView.setOnClickListener {
-            listener?.onItemClick(adapterPosition)
+            itemView.setOnClickListener {
+                val student = student ?: return@setOnClickListener
+                listener?.onItemClick(student)
+            }
         }
     }
 
     fun bind(student: Student?, position: Int) {
         this.student = student
-        nameTextView?.text = student?.name
-        idTextView?.text = student?.id
+        nameTextView.text = student?.name
+        idTextView.text = student?.idNumber
 
-        studentCheckBox?.apply {
+        studentCheckBox.apply {
             isChecked = student?.isChecked ?: false
             tag = position
+        }
+
+        val studentId = student?.id ?: return
+        viewModel.getImageUrl(studentId) {
+            Glide.with(itemView.context)
+                .load(it)
+                .into(avatar)
+            progressBar.visibility = View.GONE
         }
     }
 }
